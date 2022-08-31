749dfed1501bf22642acd93a915b963dcf3b5908
b4k4kozmo <cosmo.steiner@gmail.com>
2022-08-31 15:36:11 -0700

    player movement and collision

---
+++ src/main/GamePanel.java
@@ -0,0 +1,146 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+
+import entity.Player;
+import tile.TileManager;
+
+public class GamePanel extends JPanel implements Runnable{
+	
+	// SCREEN SETTINGS
+	final int originalTileSize = 16; //16x16 tile
+	final int scale = 3;
+	
+	public final int tileSize = originalTileSize * scale; //48x48 tile
+	public final int maxScreenCol = 16;
+	public final int maxScreenRow = 12;
+	public final int screenWidth = tileSize * maxScreenCol; //768 pixels
+	public final int screenHeight = tileSize * maxScreenRow; //576 pixels
+	
+	// WORLD SETTINGS
+	public final int maxWorldCol = 50;
+	public final int maxWorldRow = 50;
+	public final int worldWidth = tileSize * maxWorldCol;
+	public final int worldHeight = tileSize * maxWorldRow;
+	
+	// FPS
+	int FPS = 60;
+	
+	
+	TileManager tileM = new TileManager(this);
+	KeyHandler keyH = new KeyHandler();
+	Thread gameThread;
+	public CollisionChecker cChecker = new CollisionChecker(this);
+	public Player player = new Player(this,keyH);
+	
+	
+	public GamePanel () {
+		
+		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+		this.setBackground(Color.magenta);
+		this.setDoubleBuffered(true);
+		this.addKeyListener(keyH);
+		this.setFocusable(true);
+		
+	}
+	
+	public void startGameThread() {
+		
+		gameThread = new Thread(this);
+		gameThread.start();
+	}
+
+	@Override
+//	public void run() {
+//		
+//		double drawInterval = 1000000000/FPS; // 0.01666 seconds
+//		double nextDrawTime = System.nanoTime() + drawInterval; // current system time draws every draw interval
+//		
+//		while(gameThread != null) {
+//			
+//			
+//			
+//			// 1 UPDATE: Update information such as character position
+//			update();
+//			// 2 UPDATE: Draw the screen with the updated information
+//			repaint();
+//			
+//			
+//			
+//			try {
+//				double remainingTime = nextDrawTime - System.nanoTime();
+//				remainingTime = remainingTime/1000000; // convert nanosecond to millisecond
+//				
+//				if(remainingTime < 0) {
+//					remainingTime = 0;
+//				}
+//				
+//				Thread.sleep((long) remainingTime);
+//				
+//				nextDrawTime += drawInterval;
+//				
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		}
+//		
+//		
+//	}
+	
+	public void run() {
+		
+		double drawInterval = 1000000000/FPS;
+		double delta = 0;
+		long lastTime = System.nanoTime();
+		long currentTime;
+		long timer = 0;
+		int drawCount = 0;
+		
+		while(gameThread != null) {
+			
+			currentTime = System.nanoTime();
+			
+			delta += (currentTime - lastTime) / drawInterval;
+			timer += (currentTime - lastTime);
+			lastTime = currentTime;
+			
+			if (delta >= 1) {
+				update();
+				repaint();
+				delta--;
+				drawCount++;
+			}
+			if (timer >= 1000000000) {
+				System.out.println("FPS:"+drawCount);
+				drawCount = 0;
+				timer = 0;
+			}
+		}
+	}
+	
+	public void update() {
+		
+		player.update();
+		
+	}
+	public void paintComponent(Graphics g) {
+		
+		super.paintComponent(g);
+		
+		Graphics2D g2 = (Graphics2D)g;
+		
+		// .draw items are layers in top to bottom order
+		tileM.draw(g2);
+		
+		player.draw(g2);
+		
+		g2.dispose();
+	}
+
+}
